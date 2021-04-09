e31bcabd565c0489a679c3cc54d4d69361f0d95e
JULIO <noe777guillenwigetta@gmail.com>
2021-04-09 13:15:02 -0500

    tabla de mult

--- pruebaGitHub/src/pruebagithub/PruebaGitHub.java
+++ pruebaGitHub/src/pruebagithub/PruebaGitHub.java
@@ -1,11 +1,21 @@
 
 package pruebagithub;
 
+import java.util.Scanner;
+
 
 public class PruebaGitHub {
 
     public static void main(String[] args) {
+        int a;
+        Scanner entrada = new Scanner (System.in);
         System.out.println("Hola mundo...");
+        System.out.println("ingresar un numero del cual desees saber la tabla de multiplicar:");
+        a = entrada.nextInt();
+        int i = 0 ;
+        for ( i = 1; i < 11; i++) {
+            System.out.println(a + " * " + i + "= " + (a*i) );
+        }
     }
     
 }
